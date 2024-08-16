0b4f69185b44bc9f4aa933d72eee2e525e2e66db
Mika Jalkanen <mika.jalkanen@kolumbus.fi>
2024-08-16 15:44:48 +0300

    Demo testing

--- demo/skriptit.js
+++ demo/skriptit.js
@@ -2,9 +2,16 @@ let paikat = [
     [61.87421, 29.07023],
     [61.87532, 29.08011],
     [61.87720, 29.08968],
-    [61.87692, 29.09287]
+    [61.87692, 29.09287],
+    [61.87729, 29.09409],
+    [61.87426, 29.09401],
+    [61.86960, 29.10024],
+    [61.87111, 29.10224],
+    [61.87154, 29.10546]
 ], omatpaikat = [
-    [61.87878, 29.09498]
+    [61.87878, 29.09498],
+    [61.87623, 29.09375],
+    [61.86963, 29.09852]
 ]
 
 let omaPaikka, ajastin;
@@ -32,8 +39,8 @@ omatpaikat.forEach((itm) => {
 
 const omaButton1 = L.control({ position: 'topleft'});
 omaButton1.onAdd = () => {
-    const buttonDiv = L.DomUtil.create('div','leaflet-bar buttonwrapper');
-    buttonDiv.innerHTML = '<a class="leaflet-interactive">[]</a>';
+    const buttonDiv = L.DomUtil.create('div','leaflet-bar');
+    buttonDiv.innerHTML = '<a class="leaflet-interactive" style="padding-top: 6px;"><span class="material-symbols-outlined">fullscreen</span></a>';
     buttonDiv.addEventListener('click', () => {
         if (!document.fullscreenElement) {
             document.documentElement.requestFullscreen();
@@ -44,6 +51,17 @@ omaButton1.onAdd = () => {
     return buttonDiv;
 }
 omaButton1.addTo(kartta);
+
+const omaButton2 = L.control({ position: 'topleft'});
+omaButton2.onAdd = () => {
+    const buttonDiv = L.DomUtil.create('div','leaflet-bar');
+    buttonDiv.innerHTML = '<a class="leaflet-interactive" style="padding-top: 6px;"><span class="material-symbols-outlined">my_location</span></a>';
+    buttonDiv.addEventListener('click', () => {
+        haePaikka();
+    })
+    return buttonDiv;
+}
+omaButton2.addTo(kartta);
 
 function paikkaVirhe(evnt) {
     if (!omaPaikka) alert(evnt.message);
@@ -67,20 +85,26 @@ function paikkaLoytyi(evnt) {
 kartta.on('locationerror', paikkaVirhe)
 kartta.on('locationfound', paikkaLoytyi)
 
+//kartta.locate({setView: true, watch: true});
+
 function haePaikka() {
-    kartta.locate();
+    kartta.locate({setView: false, watch: true, enableHighAccuracy: true});
 }
 
-haePaikka();
+//haePaikka();
 
+// Estä ruudun "sammuminen" Chromessa
 let wakelock = null;
 
-const requestWakeLock = async () => {
-    try {
-        wakelock = await navigator.wakeLock.request();
-    } catch(err) {
-        console.error(err.name, err.message);
+if (navigator.wakeLock) {
+    const requestWakeLock = async () => {
+        try {
+            wakelock = await navigator.wakeLock.request();
+        } catch(err) {
+            console.error(err.name, err.message);
+        }
     }
+    
+    requestWakeLock();
+    
 }
-
-requestWakeLock();
