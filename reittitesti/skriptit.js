d1e9edabf5c64ee675d96099fe0fbd6c76bcd52c
Mika Jalkanen <mika.jalkanen@kolumbus.fi>
2024-08-30 14:54:01 +0300

    Route test

---
+++ reittitesti/skriptit.js
@@ -0,0 +1,392 @@
+let paikat = [
+    [61.87184544497633, 28.920806050300598],
+    [61.87128146617743, 28.921895027160648],
+    [61.87099062103433, 28.921476602554325],
+    [61.8705227339223, 28.92238318920136]
+], reitit = [
+    [
+        [61.87184544497633, 28.920806050300598]
+    ],
+    [
+        [61.87184544497633, 28.920806050300598],
+        [61.87169876090678, 28.921455144882206],
+        [61.87139527439471, 28.921551704406742],
+        [61.87128146617743, 28.921895027160648]
+    ],
+    [
+        [61.87128146617743, 28.921895027160648],
+        [61.87099062103433, 28.921476602554325]    
+    ]
+]
+
+let omaPaikka, 
+    locatePaalla = false, 
+    valittu, 
+    vanhaPaikka, 
+    pyoritysPaalla = false, 
+    haluttuSuunta, 
+    nykyinenSuunta, 
+    ajastin, 
+    debug = false, 
+    nykyinenReitti = null, 
+    triggeripiste = null,
+    seuraavaReitti = 0;
+
+let kartta = new L.map('map', {
+    center: [61.87152425652974, 28.921465873718265],
+    zoom: 16,
+    rotate: true,
+    rotateControl: {
+        closeOnZeroBearing: false
+    }
+});
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', { 
+    maxZoom: 18, 
+    attribution: '&copy <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(kartta);
+
+kartta.on('click', (evnt) => {
+    console.log(evnt.latlng.lat+', '+evnt.latlng.lng);
+    navigator.clipboard.writeText(evnt.latlng.lat+', '+evnt.latlng.lng);
+});
+
+paikat.forEach((itm) => {
+    let markeri = L.marker([itm[0],itm[1]]).addTo(kartta);
+    markeri.on('click', (evnt) => { 
+        if (valittu) poistaValinta();
+        markeri._icon.classList.add('punainen'); 
+        valittu=markeri; 
+        infotekstit(evnt.latlng,true); 
+    })
+})
+
+function paivitaReitti() {
+    if (nykyinenReitti) nykyinenReitti.removeFrom(kartta);
+    if (seuraavaReitti <= reitit.length-1) {
+        nykyinenReitti = L.polyline(reitit[seuraavaReitti], {color: 'red'}).addTo(kartta);
+        triggeripiste = reitit[seuraavaReitti].at(-1);
+        seuraavaReitti += 1;
+    } else {
+        triggeripiste = null;
+    }
+}
+
+/*
+reitit.forEach((itm) => {
+    nykyinenReitti = L.polyline(itm, {color: 'red'}).addTo(kartta);
+    triggeripiste = itm.at(-1);
+    // let prev;
+    // itm.forEach((pnt,nmbr) => {
+    //    if (nmbr == 0) {
+    //        prev = pnt;
+    //    } else {
+    //        L.polyline([[prev[0],prev[1]],[pnt[0],pnt[1]]], {color: 'red'}).addTo(kartta);
+    //        prev = pnt;
+    //    }
+    //});
+        
+    //L.polyline([[itm.slat,itm.slng],[itm.elat,itm.elng]], {color: 'red'}).addTo(kartta);
+});
+*/
+
+/*
+let omaicon = L.divIcon({ className: '', html: '<span class="material-symbols-outlined" style="font-size: 40px; transform: rotate(-45deg); opacity: 0.4;">turn_left</span>'});
+L.marker([itm[0],itm[1]], {icon: omaicon}).addTo(kartta);
+*/
+
+document.onkeyup = (e) => {
+    if (e.key=='c') {
+        kartta.setBearing(kartta.getBearing()+5);
+    }
+    if (e.key=='z') {
+        kartta.setBearing(kartta.getBearing()-5);
+    }
+    if (e.key=='x') {
+        kartta.setBearing(0);
+    }
+
+    
+}
+
+// L.polyline([[61.873355238451225, 29.094554185867313],[61.87176704495758, 29.095305204391483]], {color: 'red'}).addTo(kartta);
+
+function poistaValinta() {
+    /*
+    let onkoVihrea = false;
+    omatpaikat.forEach((oma) => {
+        if (oma[0] == valittu._latlng.lat && oma[1] == valittu._latlng.lng) {
+            onkoVihrea = true;
+        }
+    });
+    if (onkoVihrea) {
+        valittu._icon.classList.replace('punainen','vihrea')
+    } else {
+        valittu._icon.classList.remove('punainen');
+    }
+        */
+    valittu._icon.classList.remove('punainen');
+    valittu = null;
+
+}
+
+function infotekstit(laln,pun) {
+    if (laln) {
+        if (pun) {
+            document.querySelector('#lat').innerHTML = "Merkki: ";  
+            document.querySelector('#long').innerHTML = laln.lat+', '+laln.lng;
+            document.querySelector('#lat').classList.add('textpun');
+            document.querySelector('#long').classList.add('textpun');
+        } else {
+            document.querySelector('#lat').innerHTML = "Lat: " + laln.lat;
+            document.querySelector('#long').innerHTML = "Long: " + laln.lng;
+                if (document.querySelector('#lat').classList.contains('textpun')) {
+                document.querySelector('#lat').classList.remove('textpun');
+                document.querySelector('#long').classList.remove('textpun');    
+            }
+        }
+        document.querySelector('#textDiv').style.display = 'block';
+    } else {
+        if (document.querySelector('#lat').classList.contains('textpun')) {
+            poistaValinta();
+        }
+        document.querySelector('#textDiv').style.display = 'none';
+
+    }
+}
+
+
+const omaButton1 = L.control({ position: 'topleft'});
+omaButton1.onAdd = () => {
+    const buttonDiv = L.DomUtil.create('div','leaflet-bar');
+    buttonDiv.innerHTML = '<a class="leaflet-interactive" style="padding-top: 6px;"><span class="material-symbols-outlined" id="fullscreen">fullscreen</span></a>';
+    buttonDiv.addEventListener('click', () => {
+        if (!document.fullscreenElement) {
+            document.documentElement.requestFullscreen();
+            document.querySelector('#fullscreen').innerHTML = 'fullscreen_exit';
+        } else {
+            document.exitFullscreen();
+            document.querySelector('#fullscreen').innerHTML = 'fullscreen';
+        }
+    })
+    return buttonDiv;
+}
+omaButton1.addTo(kartta);
+
+const omaButton2 = L.control({ position: 'topleft'});
+omaButton2.onAdd = () => {
+    const buttonDiv = L.DomUtil.create('div','leaflet-bar');
+    buttonDiv.innerHTML = '<a class="leaflet-interactive" style="padding-top: 6px;"><span class="material-symbols-outlined" id="track">location_searching</span></a>';
+    buttonDiv.addEventListener('click', () => {
+        if (!locatePaalla) {
+            haePaikka();
+            locateButton(true);
+        } else {
+            locateButton(false);
+            kartta.stopLocate();
+        }
+    })
+    return buttonDiv;
+}
+omaButton2.addTo(kartta);
+
+const omaButton3 = L.control({position: 'topleft'});
+omaButton3.onAdd = () => {
+    const buttonDiv = L.DomUtil.create('div','leaflet-bar');
+    buttonDiv.innerHTML = '<a class="leaflet-interactive leaflet-disabled" id="info" style="padding-top: 6px;"><span class="material-symbols-outlined">info_i</span></a>';
+    buttonDiv.addEventListener('click', () => {
+        if (!document.querySelector('#info').classList.contains('leaflet-disabled')) infotekstit(omaPaikka.getLatLng(),false);
+    })
+    return buttonDiv;
+}
+omaButton3.addTo(kartta);
+
+const omaButton4 = L.control({position: 'topleft'});
+omaButton4.onAdd = () => {
+    const buttonDiv = L.DomUtil.create('div','leaflet-bar');
+    buttonDiv.innerHTML = '<a class="leaflet-interactive leaflet-disabled" id="pyoritys" style="padding-top: 6px;"><span class="material-symbols-outlined">flip_camera_android</span></a>';
+    buttonDiv.addEventListener('click', () => {
+        if (!document.querySelector('#pyoritys').classList.contains('leaflet-disabled')) {
+            if (pyoritysPaalla) {
+                pyoritysPaalla = false;
+                kartta.setBearing(0);
+                document.querySelector('#pyoritys').style.color = 'red';
+                clearInterval(ajastin);
+                ajastin = null;
+            } else {
+                pyoritysPaalla = true;
+                vanhaPaikka = omaPaikka.getLatLng();
+                document.querySelector('#pyoritys').style.color = '';
+                // init timed rotate
+                haluttuSuunta = 0; nykyinenSuunta = 0;
+                ajastin = setInterval(ajastettuKaanto,500);
+            }
+        }
+    })
+    return buttonDiv;
+}
+omaButton4.addTo(kartta);
+
+function ajastettuKaanto() {
+    if (pyoritysPaalla) {
+       
+        let nykyinen = nykyinenSuunta, haluttu = haluttuSuunta;
+        if (nykyinen >= 270 && haluttu < 90) haluttu += 360;
+        if (haluttu >= 270 && nykyinen < 90) nykyinen += 360;
+        let ero = haluttu - nykyinen;
+
+        if (ero != 0) { // haluttuSuunta != nykyinenSuunta
+            if (Math.abs(ero) <= 3) { // alle 3 asteen ero --> nykyinenSuunta = haluttuSuunta
+                kartta.setBearing(haluttu);
+                nykyinenSuunta = haluttu;
+            } else { // ero on suurempi kuin 3 astetta
+                ero = Math.round(ero / 2);
+                nykyinen += ero;                
+            }    
+        }
+        /*
+        if (nykyinen >= 360) nykyinen -= 360;
+        if (nykyinen < 0) nykyinen = 360 + nykyinen;
+
+        nykyinenSuunta = nykyinen;
+        */
+
+        nykyinenSuunta = (nykyinen < 0) ? 360 + nykyinen : (nykyinen >= 360) ? nykyinen - 360 : nykyinen;
+
+        kartta.setBearing(nykyinenSuunta);
+
+        if (debug) document.querySelector('#debug2').innerHTML = 'Nykyinen = ' + nykyinenSuunta;
+    
+    }
+}
+
+function locateButton(paalle) {
+    if (paalle) {
+        locatePaalla = true;
+        document.querySelector('#track').innerHTML = 'my_location';
+        document.querySelector('#info').classList.remove('leaflet-disabled');
+        document.querySelector('#pyoritys').classList.remove('leaflet-disabled');
+        document.querySelector('#pyoritys').style.color = 'red';
+    } else {
+        locatePaalla = false;
+        omaPaikka.removeFrom(kartta);
+        omaPaikka = null;
+        document.querySelector('#track').innerHTML = 'location_searching';
+        document.querySelector('#info').classList.add('leaflet-disabled');
+        document.querySelector('#pyoritys').classList.add('leaflet-disabled');
+        document.querySelector('#pyoritys').style.color = '';
+        pyoritysPaalla = false;
+        if (ajastin) clearInterval(ajastin);
+        kartta.setBearing(0);
+    }
+}
+
+function paikkaVirhe(evnt) {
+    if (!omaPaikka) {
+        locateButton(false);
+        alert(evnt.message);
+    }
+}
+
+function liikkuuko(vanha, uusi) {
+    if (vanha.lat.toString().slice(0,7) != uusi.lat.toString().slice(0,7)) return true;
+    if (vanha.lng.toString().slice(0,7) != uusi.lng.toString().slice(0,7)) return true;
+    return false;
+}
+
+function paivitaOmaPaikka(latlng) {
+    if (omaPaikka) { // merkki löytyy jo kartalta, siirretään sitä
+        omaPaikka.setLatLng(latlng);
+        kartta.setView(latlng);
+        
+        // tarkistetaan onko reitin triggeripiste asetettu ja ollaanko sen lähellä
+        if (triggeripiste) {
+            // ollaanko enintään 5 metrin päässä triggeripisteeltä?
+            if (kartta.distance(latlng,triggeripiste) <= 5) {
+                // etäisyys 5m tai alle, päivitetään seuraavaan reittiin
+                paivitaReitti();
+            }
+        }
+        
+
+        if (debug) document.querySelector('#debug3').innerHTML = liikkuuko(vanhaPaikka,omaPaikka.getLatLng());
+
+        if (pyoritysPaalla) {
+            if (liikkuuko(vanhaPaikka,omaPaikka.getLatLng())) {
+                let su = Math.round(suunta(vanhaPaikka,latlng));
+                haluttuSuunta = 360-su;
+
+                if (debug) document.querySelector('#debug1').innerHTML = 'Haluttu =' + haluttuSuunta;
+
+                //kartta.setBearing(360-su);
+                // Tämä toimii
+                //kartta.setBearing(-su); 
+            }
+        }
+        vanhaPaikka = latlng;    
+
+        //kartta.setView(latlng);
+
+    } else { // merkkiä omalle paikalle ei ennestään ole kartalla, lisätään se
+        let omaKuvake = L.icon({
+            iconUrl: 'radio_button_checked.png',
+            iconSize: [24,24],
+            iconAnchor: [12,12],
+            shadowUrl: 'radio_button_checked_shadow.png',
+            shadowSize: [24,24],
+            shadowAnchor: [12,12]
+        });
+
+        vanhaPaikka = latlng;
+        omaPaikka = L.marker(latlng, { icon: omaKuvake}).addTo(kartta);
+
+    }
+}
+
+function paikkaLoytyi(evnt) {
+    paivitaOmaPaikka(evnt.latlng);
+}
+
+kartta.on('locationerror', paikkaVirhe)
+kartta.on('locationfound', paikkaLoytyi)
+
+
+function haePaikka() {
+    kartta.locate({setView: false, watch: true, enableHighAccuracy: true});
+}
+
+
+function rad(asteet) {
+    return asteet * Math.PI / 180;
+}
+
+function deg(radiaanit) {
+    return radiaanit * 180 / Math.PI;
+}
+
+function suunta(alkuLatLng, loppuLatLng) {
+    let alkuLatRad = rad(alkuLatLng.lat), alkuLngRad = rad(alkuLatLng.lng), loppuLatRad = rad(loppuLatLng.lat), loppuLngRad = rad(loppuLatLng.lng);
+
+    let y = Math.sin(loppuLngRad - alkuLngRad) * Math.cos(loppuLatRad);
+    let x = Math.cos(alkuLatRad) * Math.sin(loppuLatRad) - Math.sin(alkuLatRad) * Math.cos(loppuLatRad) * Math.cos(loppuLngRad - alkuLngRad);
+    let suunta = Math.atan2(y,x);
+    suunta = deg(suunta);
+
+    return (suunta + 360) % 360;
+}
+
+// Estä ruudun "sammuminen" mobiililaitteella
+let wakelock = null;
+
+if (navigator.wakeLock) {
+    const requestWakeLock = async () => {
+        try {
+            wakelock = await navigator.wakeLock.request();
+        } catch(err) {
+            console.error(err.name, err.message);
+        }
+    }
+    
+    requestWakeLock();
+    
+}
