04d38d9e612278e737d9c19c754fcddf9a4800a9
Mika Jalkanen <mika.jalkanen@kolumbus.fi>
2025-10-24 10:17:57 +0300

    58

---
+++ 57_58/datat.js
@@ -0,0 +1,18 @@
+let paikat = [
+    [61.83414984531932, 28.980898261070255], // 58/3 Rantakaartilantie alussa
+    [61.82731736664472, 29.013159871101383], // 58/4 Nousialantie alussa
+    [61.824901070471306, 29.027574062347416], // 58/5 Hirvasjärventie AU kylän puoli
+    [61.81006986265484, 29.06367123126984], // 58/6 Särkijoentie alussa
+    [61.80445625411714, 29.07989859580994], // 58/7 Ikoinniementie alussa
+    [61.799799869750125, 29.105202538429417], // 58/8 Laakkolantie alussa
+    [61.84519052742098, 28.97875785827637], // 58/2 Hirvasjärventie alussa
+    [61.84848854302767, 28.982352018356327], // 58/1 Järvenpäänraitti alussa
+
+], 
+omatpaikat = [
+    
+],
+
+reitit = [
+
+]
